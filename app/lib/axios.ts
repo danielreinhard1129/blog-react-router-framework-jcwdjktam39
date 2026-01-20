1055f3884fd9ae6022f6d78f5c1cdb9a2ba688aa
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2026-01-20 10:50:55 +0700

    feat: add register logic (#11)

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://diversechicken-us.backendless.app",
 });
+
+export const axiosInstance2 = axios.create({
+  baseURL: "http://localhost:8000",
+});
